a2ddafb91dea2df9445ac639a27747d168ce62d5
PronkoEvgeniya <Shreidereva@gmail.com>
2022-05-05 14:26:39 +0300

    init: start work and add allkeys.js file

---
+++ src/allkeys.js
@@ -0,0 +1,568 @@
+const allkeys = {
+    backquotes: {
+        en: {
+            toLow: '`',
+            toUp: '~',
+            toCap: '`',
+        },
+        ru: {
+            toLow: 'ё',
+            toUp: 'Ё',
+            toCap: 'Ё',
+        }
+    },
+    1: {
+        en: {
+            toLow: '1',
+            toUp: '!',
+            toCap: '1',
+        },
+        ru: {
+            toLow: '1',
+            toUp: '!',
+            toCap: '1',
+        }
+    },
+    2: {
+        en: {
+            toLow: '2',
+            toUp: '@',
+            toCap: '2',
+        },
+        ru: {
+            toLow: '2',
+            toUp: '"',
+            toCap: '2',
+        }
+    },
+    3: {
+        en: {
+            toLow: '3',
+            toUp: '#',
+            toCap: '3',
+        },
+        ru: {
+            toLow: '3',
+            toUp: '№',
+            toCap: '3',
+        }
+    },
+    4: {
+        en: {
+            toLow: '4',
+            toUp: '$',
+            toCap: '4',
+        },
+        ru: {
+            toLow: '4',
+            toUp: ';',
+            toCap: '4',
+        }
+    },
+    5: {
+        en: {
+            toLow: '5',
+            toUp: '%',
+            toCap: '5',
+        },
+        ru: {
+            toLow: '5',
+            toUp: '%',
+            toCap: '5',
+        }
+    },
+    6: {
+        en: {
+            toLow: '6',
+            toUp: '^',
+            toCap: '6',
+        },
+        ru: {
+            toLow: '6',
+            toUp: ':',
+            toCap: '6',
+        }
+    },
+    7: {
+        en: {
+            toLow: '7',
+            toUp: '&',
+            toCap: '7',
+        },
+        ru: {
+            toLow: '7',
+            toUp: '?',
+            toCap: '7',
+        }
+    },
+    8: {
+        en: {
+            toLow: '8',
+            toUp: '*',
+            toCap: '8',
+        },
+        ru: {
+            toLow: '8',
+            toUp: '*',
+            toCap: '8',
+        }
+    },
+    9: {
+        en: {
+            toLow: '9',
+            toUp: '(',
+            toCap: '9',
+        },
+        ru: {
+            toLow: '9',
+            toUp: '(',
+            toCap: '9',
+        }
+    },
+    0: {
+        en: {
+            toLow: '0',
+            toUp: ')',
+            toCap: '0',
+        },
+        ru: {
+            toLow: '0',
+            toUp: ')',
+            toCap: '0',
+        }
+    },
+    dash: {
+        en: {
+            toLow: '-',
+            toUp: '_',
+            toCap: '-',
+        },
+        ru: {
+            toLow: '-',
+            toUp: '_',
+            toCap: '-',
+        }
+    },
+    plus: {
+        en: {
+            toLow: '=',
+            toUp: '+',
+            toCap: '=',
+        },
+        ru: {
+            toLow: '=',
+            toUp: '+',
+            toCap: '=',
+        }
+    },
+    q: {
+        en: {
+            toLow: 'q',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    w: {
+        en: {
+            toLow: 'w',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    e: {
+        en: {
+            toLow: 'e',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    r: {
+        en: {
+            toLow: 'r',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    t: {
+        en: {
+            toLow: 't',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    y: {
+        en: {
+            toLow: 'y',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    u: {
+        en: {
+            toLow: 'u',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    i: {
+        en: {
+            toLow: 'i',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    o: {
+        en: {
+            toLow: 'o',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: '',
+            toCap: '',
+        }
+    },
+    p: {
+        en: {
+            toLow: 'p',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: '',
+            toUp: 'Ё',
+            toCap: 'Ё',
+        }
+    },
+    leftbrecket: {
+        en: {
+            toLow: '[',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: 'ё',
+            toUp: 'Ё',
+            toCap: 'Ё',
+        }
+    },
+    rightbracket: {
+        en: {
+            toLow: ']',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: 'ё',
+            toUp: 'Ё',
+            toCap: 'Ё',
+        }
+    },
+    line: {
+        en: {
+            toLow: '\\',
+            toUp: '',
+            toCap: '',
+        },
+        ru: {
+            toLow: 'ё',
+            toUp: 'Ё',
+            toCap: 'Ё',
+        }
+    },
+    a: {
+        en: {
+            toLow: 'a',
+            toUp: 'A',
+            toCap: 'A',
+        },
+        ru: {
+            toLow: 'ф',
+            toUp: 'Ф',
+            toCap: 'Ф',
+        }
+    },
+    s: {
+        en: {
+            toLow: 's',
+            toUp: 'S',
+            toCap: 'S',
+        },
+        ru: {
+            toLow: 'ы',
+            toUp: 'Ы',
+            toCap: 'Ы',
+        }
+    },
+    d: {
+        en: {
+            toLow: 'd',
+            toUp: 'D',
+            toCap: 'D',
+        },
+        ru: {
+            toLow: 'в',
+            toUp: 'В',
+            toCap: 'В',
+        }
+    },
+    f: {
+        en: {
+            toLow: 'f',
+            toUp: 'F',
+            toCap: 'F',
+        },
+        ru: {
+            toLow: 'а',
+            toUp: 'А',
+            toCap: 'А',
+        }
+    },
+    g: {
+        en: {
+            toLow: 'g',
+            toUp: 'G',
+            toCap: 'G',
+        },
+        ru: {
+            toLow: 'п',
+            toUp: 'П',
+            toCap: 'П',
+        }
+    },
+    h: {
+        en: {
+            toLow: 'h',
+            toUp: 'H',
+            toCap: 'H',
+        },
+        ru: {
+            toLow: 'р',
+            toUp: 'Р',
+            toCap: 'Р',
+        }
+    },
+    j: {
+        en: {
+            toLow: 'j',
+            toUp: 'J',
+            toCap: 'J',
+        },
+        ru: {
+            toLow: 'о',
+            toUp: 'О',
+            toCap: 'О',
+        }
+    },
+    k: {
+        en: {
+            toLow: 'k',
+            toUp: 'K',
+            toCap: 'K',
+        },
+        ru: {
+            toLow: 'л',
+            toUp: 'Л',
+            toCap: 'Л',
+        }
+    },
+    l: {
+        en: {
+            toLow: 'l',
+            toUp: 'L',
+            toCap: 'L',
+        },
+        ru: {
+            toLow: 'д',
+            toUp: 'Д',
+            toCap: 'Д',
+        }
+    },
+    colon: {
+        en: {
+            toLow: ';',
+            toUp: ':',
+            toCap: ';',
+        },
+        ru: {
+            toLow: 'ж',
+            toUp: 'Ж',
+            toCap: 'Ж',
+        }
+    },
+    quote: {
+        en: {
+            toLow: "'",
+            toUp: '"',
+            toCap: "'",
+        },
+        ru: {
+            toLow: 'э',
+            toUp: 'Э',
+            toCap: 'Э',
+        }
+    },
+    z: {
+        en: {
+            toLow: 'z',
+            toUp: 'Z',
+            toCap: 'Z',
+        },
+        ru: {
+            toLow: 'я',
+            toUp: 'Я',
+            toCap: 'Я',
+        }
+    },
+    x: {
+        en: {
+            toLow: 'x',
+            toUp: 'X',
+            toCap: 'X',
+        },
+        ru: {
+            toLow: 'ч',
+            toUp: 'Ч',
+            toCap: 'Ч',
+        }
+    },
+    c: {
+        en: {
+            toLow: 'c',
+            toUp: 'C',
+            toCap: 'C',
+        },
+        ru: {
+            toLow: 'с',
+            toUp: 'С',
+            toCap: 'С',
+        }
+    },
+    v: {
+        en: {
+            toLow: 'v',
+            toUp: 'V',
+            toCap: 'V',
+        },
+        ru: {
+            toLow: 'м',
+            toUp: 'М',
+            toCap: 'М',
+        }
+    },
+    b: {
+        en: {
+            toLow: 'b',
+            toUp: 'B',
+            toCap: 'B',
+        },
+        ru: {
+            toLow: 'и',
+            toUp: 'И',
+            toCap: 'И',
+        }
+    },
+    n: {
+        en: {
+            toLow: 'n',
+            toUp: 'N',
+            toCap: 'N',
+        },
+        ru: {
+            toLow: 'т',
+            toUp: 'Т',
+            toCap: 'Т',
+        }
+    },
+    m: {
+        en: {
+            toLow: 'm',
+            toUp: 'M',
+            toCap: 'M',
+        },
+        ru: {
+            toLow: 'ь',
+            toUp: 'Ь',
+            toCap: 'Ь',
+        }
+    },
+    comma: {
+        en: {
+            toLow: ',',
+            toUp: '<',
+            toCap: ',',
+        },
+        ru: {
+            toLow: 'б',
+            toUp: 'Б',
+            toCap: 'Б',
+        }
+    },
+    dot: {
+        en: {
+            toLow: '.',
+            toUp: '>',
+            toCap: '.',
+        },
+        ru: {
+            toLow: 'ю',
+            toUp: 'Ю',
+            toCap: 'Ю',
+        }
+    },
+    question: {
+        en: {
+            toLow: '/',
+            toUp: '?',
+            toCap: '/',
+        },
+        ru: {
+            toLow: '.',
+            toUp: ',',
+            toCap: '.',
+        }
+    },
+}
+
+export default allkeys;
